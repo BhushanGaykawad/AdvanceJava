0533fdff1084b4cfdc5d2f5347adbf14003370f9
BhushanGaykawad <148471066+BhushanGaykawad@users.noreply.github.com>
2023-12-23 18:06:17 +0530

    Add files via upload

---
+++ Filter/FirstFilter.java
@@ -0,0 +1,64 @@
+package mypack;
+import java.io.*;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet Filter implementation class FirstFilter
+ */
+public class FirstFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public FirstFilter() 
+    {
+        super();
+        
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() 
+	{
+		
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
+	{
+		String name=request.getParameter("nm");
+		String age=request.getParameter("ag");
+		FileWriter fw=new FileWriter("d:\\loginfile.txt");
+		fw.write(name);
+		fw.write(age);
+		fw.close();
+		PrintWriter pw=response.getWriter();
+		pw.println("<br>Data saved in a file<br>");
+		
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException 
+	{
+		
+	}
+
+}
