528d2fd7221f3005c853bc95a084e320a4a28298
BhushanGaykawad <148471066+BhushanGaykawad@users.noreply.github.com>
2023-12-23 17:53:58 +0530

    Add files via upload

---
+++ loadOnStartup/FirstServ.java
@@ -0,0 +1,32 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServ
+ */
+public class FirstServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	static
+	{
+		System.out.println("First servlet is loaded.");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		PrintWriter pw= response.getWriter();
+		pw.println("Welcome in First Serv");
+		
+	}
+
+}
