e6d65c7eb1bcdc92476618fdd942554b4a483da6
BhushanGaykawad <148471066+BhushanGaykawad@users.noreply.github.com>
2023-12-23 18:07:47 +0530

    Add files via upload

---
+++ Maven/App.java
@@ -0,0 +1,13 @@
+package master.com.vita;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
